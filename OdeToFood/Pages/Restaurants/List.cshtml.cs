1a46af6ed02ce3fd09364f8a6620a70f47a3cac6
QuangdaoN <quangdaonguyen@graydientcreative.com>
2019-03-26 15:27:21 -0500

    Model binding for search

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using OdeToFood.Core;
 using OdeToFood.Data;
+using System.Collections.Generic;
 
 namespace OdeToFood.Pages.Restaurants
 {
@@ -16,19 +13,22 @@ namespace OdeToFood.Pages.Restaurants
         private readonly IRestaurantData restaurantData;
 
         public string Message { get; set; }
-        public IEnumerable<Restaurant> Restaurants {get;set;}
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        public ListModel (IConfiguration config, 
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
+        public ListModel(IConfiguration config,
                           IRestaurantData restaurantData)
         {
             this.config = config;
             this.restaurantData = restaurantData;
         }
 
-        public void OnGet(string searchTerm)
+        public void OnGet()
         {
             Message = config["Message"];
-            Restaurants = restaurantData.GetRestaurantsByName(searchTerm);
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
